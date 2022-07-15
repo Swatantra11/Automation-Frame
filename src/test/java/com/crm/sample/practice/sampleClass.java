0b33d9ae3aa0b9c2aa3ca76c9b1dc1ad195d5f67
Rishikesh <Rishikesh@DESKTOP-0CCPETD>
2022-07-15 15:50:12 +0530

    my first project repo

---
+++ src/test/java/com/crm/sample/practice/sampleClass.java
@@ -0,0 +1,16 @@
+package com.crm.sample.practice;
+
+import org.testng.annotations.Test;
+
+public class sampleClass {
+@Test(alwaysRun=true)
+	public void sample1() {
+		
+			
+System.out.println("test case 1");
+	}
+@Test(alwaysRun=true)
+public void sample2() {
+	System.out.println("test case 2");
+}
+}
